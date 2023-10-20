54e04b78c49fe038a7d62687ef66c988aef8bc63
Drew Banyai <DrewBanyai@gmail.com>
2023-10-20 15:09:36 -0400

    Updated the "next build" post build script to properly update the 404 page in addition to the index

--- FrontEnd_MainWebsite/public/postBuild.py
+++ FrontEnd_MainWebsite/public/postBuild.py
@@ -1,14 +1,23 @@
 import os
 
 #  Read in the index.html file to to a variable
-html = None
+indexHTML = None
 with open("./index.html", "r") as f:
-     html = f.read()
+     indexHTML = f.read()
      f.close()
 
-#  Rename the _next filder and change all references to it as well
+#  Read in the 404.html file to to a variable
+fourOhFourHTML = None
+with open("./404.html", "r") as f:
+     fourOhFourHTML = f.read()
+     f.close()
+
+#  Change all references in the HTML from /_next to /next
+indexHTML = indexHTML.replace('/_next', './next')
+fourOhFourHTML = fourOhFourHTML.replace('/_next', './next')
+
+#  Rename the /_next folder to /next (if it exists)
 try:
-     html = html.replace('/_next', './next')
      os.rename('./_next/', './next/')
 except Exception as e:
      print("File Not Found: './_next'. Skipping step...")
@@ -20,11 +29,19 @@ for dirpath, dirs, files in os.walk("."):
                print("replacing [" + dirpath + "\\" +  file + "] with [" + dirpath + "\\" +  file[1:len(file)] + "]")
                forwardSlash1 = (dirpath + "\\" +  file).replace("\\", "/")
                forwardSlash2 = (dirpath + "\\" +  file[1:len(file)]).replace("\\", "/")
-               html = html.replace(dirpath + "\\" +  file, dirpath + "\\" +  file[1:len(file)])
-               html = html.replace(forwardSlash1, forwardSlash2)
                os.rename(dirpath + "\\" +  file, dirpath + "\\" +  file[1:len(file)])
 
-#  Write the altered data back to the index.html file
+               indexHTML = indexHTML.replace(dirpath + "\\" +  file, dirpath + "\\" +  file[1:len(file)])
+               indexHTML = indexHTML.replace(forwardSlash1, forwardSlash2)
+               fourOhFourHTML = fourOhFourHTML.replace(dirpath + "\\" +  file, dirpath + "\\" +  file[1:len(file)])
+               fourOhFourHTML = fourOhFourHTML.replace(forwardSlash1, forwardSlash2)
+
+#  Write the altered data back to the index HTML file
 with open("./index.html", 'w') as f:
-    f.write(html)
+    f.write(indexHTML)
+    f.close()
+    
+#  Write the altered data back to the 404 HTML file
+with open("./404.html", 'w') as f:
+    f.write(fourOhFourHTML)
     f.close()
